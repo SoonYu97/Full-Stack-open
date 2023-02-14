49226f79ab50c4b13c3d2166a335a57513fb411e
Soon Yu <yusoon.97417@gmail.com>
2023-02-14 20:26:14 +0800

    Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,33 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  const { good, neutral, bad, sum, average, positive } = props;
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {sum}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const IncreaseGood = () => setGood(good + 1)
-  const IncreaseNeutral = () => setNeutral(neutral + 1)
-  const IncreaseBad = () => setBad(bad + 1)
+  const IncreaseGood = () => setGood(good + 1);
+  const IncreaseNeutral = () => setNeutral(neutral + 1);
+  const IncreaseBad = () => setBad(bad + 1);
 
-  const sum = good + bad + neutral
-  const average = (good - bad) / sum
-  const positive = good / sum * 100
+  const sum = good + bad + neutral;
+  const average = (good - bad) / sum;
+  const positive = (good / sum) * 100;
 
   return (
     <>
@@ -22,15 +37,14 @@ const App = () => {
         <button onClick={IncreaseNeutral}>neutral</button>
         <button onClick={IncreaseBad}>bad</button>
       </div>
-      <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {sum}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        sum={sum}
+        average={average}
+        positive={positive}
+      />
     </>
   );
 };
