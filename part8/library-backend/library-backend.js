fab1d2a83531ee1059495159ceecb096dd62a7a8
SoonYu97 <yusoon.97417@gmail.com>
2023-03-21 00:05:20 +0800

    feat(Exercise 8.5): add optional genre parameter

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -90,7 +90,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -115,8 +115,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter((book) => book.author === args.author)
+      if (!args.author && !args.genre) return books
+      if (!args.genre)
+        return books.filter((book) => book.author === args.author)
+      if (!args.author)
+        return books.filter((book) => book.genres.includes(args.genre))
+      return books.filter((book) => book.genres.includes(args.genre) && book.author === args.author)
     },
     allAuthors: () => authors,
   },
