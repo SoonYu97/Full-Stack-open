7036907e2f676e5cf51c8c0159913442fad7aafd
SoonYu97 <yusoon.97417@gmail.com>
2023-03-21 00:34:14 +0800

    refactor: prepare for Exercise 8.8-8.12

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom"
+
+import Authors from "./components/Authors"
+import Books from "./components/Books"
+import NewBook from "./components/NewBook"
+
+const App = () => {
+
+	return (
+		<div>
+			<div>
+        <Link to="/">authors</Link>
+        <Link to="/books">books</Link>
+        <Link to="/add">add book</Link>
+			</div>
+
+      <Routes>
+        <Route path="/" element={<Authors />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/add" element={<NewBook />} />
+      </Routes>
+		</div>
+	)
+}
+
+export default App
