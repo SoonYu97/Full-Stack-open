dfaa1945aff1249c7c546921f24f5c0f74a3c557
SoonYu97 <yusoon.97417@gmail.com>
2023-03-21 00:37:18 +0800

    feat(Exercise 8.8): add authors view

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,5 +1,21 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
 const Authors = () => {
-	const authors = []
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
 	return (
 		<div>
@@ -11,7 +27,7 @@ const Authors = () => {
 						<th>born</th>
 						<th>books</th>
 					</tr>
-					{authors.map((a) => (
+					{result.data.allAuthors.map((a) => (
 						<tr key={a.name}>
 							<td>{a.name}</td>
 							<td>{a.born}</td>
