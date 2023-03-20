19ebfe6eeb30b8223a5945c9c21d18a2583824ef
SoonYu97 <yusoon.97417@gmail.com>
2023-03-20 23:56:13 +0800

    feat(Exercise 8.3): add query for all authors

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -91,11 +91,13 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 
   type Author {
     name: String!
     born: Int
+    bookCount: Int!
     id: ID!
   }
 
@@ -112,7 +114,15 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    name: (root) => root.name,
+    born: (root) => root.born,
+    id: (root) => root.id,
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   }
 }
 
